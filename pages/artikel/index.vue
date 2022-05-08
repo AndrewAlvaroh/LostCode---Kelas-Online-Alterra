a32fc67e9d3ed5e4d1e65b61cf361836052ba68c
AndrewAlvaroh <andrewalvaroh@gmail.com>
2022-05-08 21:56:24 +0700

    based pages

---
+++ pages/artikel/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>artikel</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ pages/kelas/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>kelas</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
